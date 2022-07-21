c925c359c77962c20007221a06c15d716047f91c
Magics422 <magics422@gmail.com>
2022-07-21 19:21:13 +1200

    Numbr test

---
+++ test.py
@@ -0,0 +1,12 @@
+number1 =4
+number2 =6
+result = number1 + number2
+print (result) # test number
+
+num1, num2 = 3, 6
+print (num1, num2) # test many number
+
+swap1 = 4
+swap2 = 7
+swap1, swap2 = swap2, swap1
+print (swap1, swap2) # test swap
